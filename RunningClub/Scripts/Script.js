456d3bb4ada4df845758558f167b661ac9886902
IamTonyZHOU <beyondjay@gmail.com>
2015-09-23 14:15:05 +0800

    add angular js
    add angular js

---
+++ RunningClub/Scripts/Script.js
@@ -0,0 +1,33 @@
+﻿var clubApp = angular.module('clubApp', ['ngRoute']);
+
+// configure our routes
+clubApp.config(function ($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
+        })
+
+        // route for the about page
+        .when('/track', {
+            templateUrl: 'pages/track.html'
+        })
+
+       
+});
+
+// create the controller and inject Angular's $scope
+clubApp.controller('mainController', function ($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+clubApp.controller('trackController', function ($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+clubApp.controller('contactController', function ($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
